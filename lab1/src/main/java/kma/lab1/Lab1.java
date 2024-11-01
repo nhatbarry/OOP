35c07c02ccee10f37f0fdc2fd6c805596aae73a7
nhatbarry <nhatbarry@gmail.com>
2024-11-01 16:07:41 +0700

    done bai 1 lab 1

--- lab1/src/main/java/kma/lab1/Lab1.java
+++ lab1/src/main/java/kma/lab1/Lab1.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package kma.lab1;
 
-/**
- *
- * @author nhatb
- */
 public class Lab1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.printf("Le Anh Nhat\tCT070335\tCT7C\tnhatbarry@gmail.com");
+        System.out.println();
+        for (int i = 9; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println("No more bottles of beer on the wall.");
+            }
+            else
+            System.out.printf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down, pass it around,\n", i, i);
+        }
     }
 }
