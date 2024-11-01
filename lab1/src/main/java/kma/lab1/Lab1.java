94b99552dec704bd5b5a4d1ef5a1dc38583f0199
nhatbarry <nhatbarry@gmail.com>
2024-11-01 16:01:51 +0700

    new lab 1

---
+++ lab1/src/main/java/kma/lab1/Lab1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kma.lab1;
+
+/**
+ *
+ * @author nhatb
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
